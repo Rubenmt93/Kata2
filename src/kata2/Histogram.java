b85acc2d848e0eb737e054a9aa9cafaa8b8d4f09
zegre <zegre@DESKTOP-FV3D7OF>
2017-11-03 13:49:36 +0000

    Quinta versión. Se introduce un metodo por el cual el usuario puede llenar el vector con el tamaño y numero que el quiera

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,16 +3,30 @@ package kata2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.function.Consumer;
 
  
 public class Histogram<T> {
-    private final T[] data;
+    private T[] data;
     Map<Integer, Integer> histogram= new HashMap<>();   
     
-    public Histogram(T[] a){
-        this.data=a;
+    public Histogram(){
+        this.data= (T[]) new Object [] {};
     }
+    
+    public void fillData(){
+        System.out.println("Tamaño del vector:");
+        Scanner input = new Scanner(System.in);
+        Object [] aux;
+        aux = new Object[input.nextInt()];
+        for(int i=0; i<aux.length; i++){
+            System.out.println("Introduzca elemento número:" + (i+1));
+            aux[i] = input.nextInt();
+        }
+        data= (T[]) aux;
+    }
+    
 
     public void fill(){
         for(int i=0; i<data.length; i++){
