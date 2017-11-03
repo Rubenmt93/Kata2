a6169dfc1bab0e4faed10e4d680a4b7e68b3ecd6
zegre <zegre@DESKTOP-FV3D7OF>
2017-11-03 13:27:57 +0000

    Cuarta version. Se introduce el concepto de clase genérica y tipo genérico

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -6,21 +6,21 @@ import java.util.Map;
 import java.util.function.Consumer;
 
  
-public class Histogram {
-    private final int[] data;
+public class Histogram<T> {
+    private final T[] data;
     Map<Integer, Integer> histogram= new HashMap<>();   
     
-    public Histogram(){
-        this.data=new int[] {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+    public Histogram(T[] a){
+        this.data=a;
     }
 
     public void fill(){
         for(int i=0; i<data.length; i++){
            
             if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
+                histogram.put((Integer) data[i], histogram.get(data[i])+1);
             }else{
-                histogram.put(data[i], 1);
+                histogram.put((Integer) data[i], 1);
             }
          
         
