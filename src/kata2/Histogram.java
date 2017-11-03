27979a368729d42ed449974a4a2017021684e2e6
zegre <zegre@DESKTOP-FV3D7OF>
2017-11-03 13:03:56 +0000

    Tercera version. En esta versión creamos metodos dentro del objeto histogram para que sean ellos los ecargados e realizar las diferentes tareas.

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -14,7 +14,7 @@ public class Histogram {
         this.data=new int[] {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
     }
 
-    public void hacer(){
+    public void fill(){
         for(int i=0; i<data.length; i++){
            
             if(histogram.containsKey(data[i])){
@@ -25,6 +25,9 @@ public class Histogram {
          
         
         }      
+        
+    }
+    public void print(){    
         histogram.keySet().forEach((val) -> {
             System.out.println(" Valor: " + val +" Veces repetidos: " + histogram.get(val));
         });
